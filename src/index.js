cdaebe40f9dc355f395d78b7747a14bb4303b45e
broleomo <brooklynnmoor@gmail.com>
2017-09-21 11:39:13 -0400

    first try

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import './styles/App.css';
+import registerServiceWorker from './registerServiceWorker';
+
+
+// IMPORT REACT ROUTER <<<<<<<<<<<<<<<<<<<<<<
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+// IMPORT COMPONENTS <<<<<<<<<<<<<<<<<<<<<<
+import Explorer from './components/Explorer';
+import Films from './components/Films';
+import BaseLayout from './components/Layout';
+import People from './components/People';
+import Starships from './components/Starships';
+
+ReactDOM.render(
+<BrowserRouter>
+  <BaseLayout>
+    <Switch>
+      <Route exact path="/" components={People}/>
+      <Route path="/films" component={Films}/>
+      <Route path="/characters" component={People}/>
+      <Route path="/starships" components={Starships}/>
+
+    </Switch>
+  </BaseLayout>
+</BrowserRouter>
+
+
+
+,document.getElementById('root'));
+registerServiceWorker();
+{/* >>>>>>>>>>>>>>>>>>>>>
+  Add a router components with routes to:
+  Explorer. Use an exact path to "/"
+  Starships
+  Films
+  People
+  <<<<<<<<<<<<<<<<<<<<<<
+  */}
